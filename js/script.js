7de0ec203744cfcd0dfef831e4531e55cfb48753
Karyna <88458417+DevGirl20@users.noreply.github.com>
2023-02-17 16:39:41 +0200

    1_js
    js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//1
+
+let name = prompt("Напиши свое имя и фамилию");
+alert(`Привет ${name}`);
+
+//2
+const yearNow = 2023;
+let whatYourAge = prompt("Напиши какого ты года рождения");
+alert(`Тебе ${yearNow - whatYourAge}`);
+
+
+//3
+let length = prompt("Я помогу посчитать периметр квадрата - напиши длину стороны квадрата");
+alert(`периметр твоего квадрата -  ${4 * length}`);
+
+
